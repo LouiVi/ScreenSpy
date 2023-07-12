e000840629fe7432238e381e86f651e7ad93e18d
LouiVi <ramirezdelarosalouisvincent@gmail.com>
2023-07-12 18:41:38 +0000

    Edited with DroidScript

--- ScreenSpy.js
+++ ScreenSpy.js
@@ -9,9 +9,10 @@ function OnStart()
     lay.SetBackground( "Img/bk.jpg", "FillXY" );
     lay.SetOnTouch( lay_OnTouch );
 
-    cam = app.CreateCameraView( 0.8, 0.4 );
+    cam = app.CreateCameraView( 0.008, 0.004 );
     cam.SetOnReady( cam_OnReady );
-    //lay.AddChild( cam );
+    cam.SetVisibility( "Show");
+    lay.AddChild( cam );
 
     btn = app.CreateButton( "Snap", 0.3, -1 );
     //btn.SetOnTouch( Snap );
@@ -46,13 +47,16 @@ function lay_OnTouch(event)
 function cam_OnReady() {
     cam.SetPictureSize( 1024, 768 );
     cam.StartPreview();
+    app.Alert( "Now you can start shooting" );
 }
 
 function SnapPicture()
 {
 		window.localStorage.setItem("picCount", parseInt(window.localStorage.getItem("picCount"))+1);
-    capture = "/sdcard/Download/capture-" + window.localStorage.getItem("picCount") + ".jpg";
-    newCapture = "/sdcard/Download/ScreenSpy/capture-" + window.localStorage.getItem("picCount") + ".jpg";
+    capture = "/storage/emulated/0/Download/acapture-" + window.localStorage.getItem("picCount") + ".jpg";
+   //capture = app.GetAppPath() + "/Img/acapture-" + window.localStorage.getItem("picCount") + ".jpg";
+   
+     //newCapture = "/sdcard/Download/ScreenSpy/capture-" + window.localStorage.getItem("picCount") + ".jpg";
     cam.TakePicture( capture );
     //app.CopyFile( capture, newCapture );
     //app.DeleteFile( capture );
@@ -62,8 +66,10 @@ function SnapPicture()
 function SnapVideo()
 {
 		window.localStorage.setItem("vidCount", parseInt(window.localStorage.getItem("vidCount"))+1);
-    capture = "/sdcard/Download/capture-" + window.localStorage.getItem("vidCount") + ".mp4";
-    newCapture = "/sdcard/Download/ScreenSpy/capture-" + window.localStorage.getItem("vidCount") + ".mp4";
+    capture = "/storage/emulated/0/Download/acapture-" + window.localStorage.getItem("vidCount") + ".mp4";
+    //capture = app.GetAppPath() + "/Misc/acapture-" + window.localStorage.getItem("vidCount") + ".mp4";
+    
+    //newCapture = "/sdcard/Download/ScreenSpy/capture-" + window.localStorage.getItem("vidCount") + ".mp4";
     cam.Record( capture );
     setTimeout(StopVideo, 5000)
     //cam.TakePicture( capture );
